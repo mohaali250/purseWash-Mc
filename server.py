0cd981097761697b9d651d79f658b56d979e7cd0
mohaali250 <wsedrftgxcvb2@gmail.com>
2026-04-21 15:05:56 +0100

    Rename on_join to on_login for clarity

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ def is_ok():
             return not cron
 
 @EventHandler
-def on_join(event):
+def on_login(event):
     now = datetime.datetime.now(datetime.timezone.utc).time()
     if is_ok() and (datetime.time(9,0) <= now <= datetime.time(15,0)):
         event.disallow("Ops... The server isnt on a 24/7 plan right now. Come back at 15:00 UTC+0. To check the predicted schedule, go to our discord server and download the schedule prediction script.")
