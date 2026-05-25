43db00070337190faf41f5d6b52d8950b9f5f030
mohaali250 <wsedrftgxcvb2@gmail.com>
2026-05-25 20:49:20 +0100

    Refactor random_tips.py for improved readability

--- server/random_tips.py
+++ server/random_tips.py
@@ -1,97 +1,85 @@
 # -*- coding: utf-8 -*-
-import datetime, json, random, time
+
+import json,random
 
 from java.net import URL
-from java.io import BufferedReader, InputStreamReader
 from java.util import Scanner
 
-from org.bukkit import Bukkit
-from org.bukkit import ChatColor
-from java.lang import Runnable
+from org.bukkit import Bukkit,ChatColor
 
-URL_DATA = "https://raw.githubusercontent.com/mohaali250/purseWash-Mc/refs/heads/main/data/random_tips.json"
+from net.md_5.bungee.api.chat import TextComponent
+from net.md_5.bungee.api.chat import HoverEvent
+from net.md_5.bungee.api.chat import ClickEvent
+from net.md_5.bungee.api.chat.hover.content import Text
+
+URL_DATA="https://raw.githubusercontent.com/mohaali250/purseWash-Mc/refs/heads/main/data/random_tips.json"
 
 def fetch_data():
-    try:
-        stream = URL(URL_DATA).openStream()
-        scanner = Scanner(stream).useDelimiter("\\A")
-        response = scanner.next() if scanner.hasNext() else None
-        scanner.close()
 
-        if response is None or response.strip() == "":
-            print("Empty response from URL")
+    try:
+        stream=URL(URL_DATA).openStream()
+        scanner=Scanner(stream).useDelimiter("\\A")
+        response=scanner.next() if scanner.hasNext() else None
+        scanner.close()
+        if response is None:
             return None
 
-        print("RAW RESPONSE:", response[:200])
         return json.loads(response)
-
-    except Exception as e:
-        print("fetch_data error:", e)
+    except Exception as ex:
+        Bukkit.getLogger().severe(
+            "[RandomTips] {}".format(ex)
+        )
         return None
 
-def chcol(msg):
-    return ChatColor.translateAlternateColorCodes('&', msg)
-
+def cc(msg):
+    return ChatColor.translateAlternateColorCodes('&',msg)
 
 def randomtip():
-
     try:
-
-        tip = random.choice(data["tips"])
-
-        parts = []
-
-        parts.append(
-            "\"text\":\"" + tip["content"].replace("\"","\\\"") + "\""
+        tip=random.choice(data["tips"])
+        msg=TextComponent(
+            cc(tip["content"])
         )
-
         if len(tip["tooltip"]) != 0:
-
-            parts.append(
-                "\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"" +
-                tip["tooltip"].replace("\"","\\\"") +
-                "\"}"
+            msg.setHoverEvent(
+                HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT,
+                    [Text(cc(tip["tooltip"]))]
+                )
             )
-
         if len(tip["do"].keys()) != 0:
+            action=list(tip["do"].keys())[0]
+            value=tip["do"][action]
+            click_action={
+                "run_command":ClickEvent.Action.RUN_COMMAND,
+                "suggest_command":ClickEvent.Action.SUGGEST_COMMAND,
+                "open_url":ClickEvent.Action.OPEN_URL
+            }.get(action)
+            if click_action is not None:
+                msg.setClickEvent(
+                    ClickEvent(
+                        click_action,
+                        value
+                    )
+                )
 
-            action = list(tip["do"].keys())[0]
-            value = tip["do"][action]
-
-            parts.append(
-                "\"clickEvent\":{\"action\":\"" +
-                action +
-                "\",\"value\":\"" +
-                value.replace("\"","\\\"") +
-                "\"}"
-            )
-
-        cmd = "/tellraw @a {" + ",".join(parts) + "}"
-
-        print(cmd)
-
-        Bukkit.dispatchCommand(
-            Bukkit.getConsoleSender(),
-            cmd
-        )
-
+        for p in Bukkit.getOnlinePlayers():
+            p.spigot().sendMessage(msg)
     except Exception as ex:
-
         Bukkit.getLogger().severe(
             "[RandomTips] {}".format(ex)
         )
 
-plugin = Bukkit.getPluginManager().getPlugin("PySpigot")
-data = fetch_data()
+plugin=Bukkit.getPluginManager().getPlugin("PySpigot")
 
-# Run
-
-if data is None:
-    print("Nothing returned")
-else:
+data=fetch_data()
+if data is not None:
     Bukkit.getScheduler().runTaskTimer(
         plugin,
         randomtip,
         0,
-        20 * 300
+        20*300
     )
+    print("[RandomTips] Loaded.")
+else:
+    print("[RandomTips] Failed to load data.")
