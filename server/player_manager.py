8402c2ca4a820ae93de90fa41c29649f358ef8bf
mohaali250 <wsedrftgxcvb2@gmail.com>
2026-06-11 00:08:48 +0100

    Refactor player punishment handling logic

--- server/player_manager.py
+++ server/player_manager.py
@@ -366,21 +366,27 @@ def onCommand(sender,label,args):
         d=data[u]
 
         #Run start
+
+        _any = False
+
         if d["banned"] != 0:
+            _any = True
             dur = now() - d["banned"]
             chat_log(sender,3,"Lifted staff ban of %s which left %s to finish.",variables=(args[0],str(pretty_timedelta(dur) if dur != -1 else "Never")))
             chat_log(target,4,"%s ban from staff is now lifted. Reagree to rules by typing [/activate staff]. More info on [/status]",variables=("Your"))
             d["staff_playtime"] = 0
             d["banned"]=now()
-        elif 0 < d["locked"]:
+        if 0 < d["locked"]:
+            _any = True
             dur = now() - d["banned"]
             chat_log(sender,3,"Lifted staff suspension of %s which left %s to finish.",variables=(args[0],str(pretty_timedelta(dur) if dur != -1 else "Never")))
             chat_log(target,4,"%s staff suspension is now lifted. Reagree to rules by typing [/activate staff]. More info on [/status]",variables=("Your"))
             d["locked"] = -2
-        elif _bit.read(d["notify"],6):
+        if _bit.read(d["notify"],6):
+            _any = True
             d["notify"] = _bit.write(d["notify"],6,False)
             chat_log(sender,3,"Cancelled %s's inventory clear.",variables=(args[0]))
-        else:
+        if not _any:
             chat_log(sender,3,"%s doesnt have a punishment to remove",variables=(args[0]))
 
         local_session_notify[uuid(sender)]=0
