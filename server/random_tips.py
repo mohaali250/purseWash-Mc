a17d266e2807e3860fb74aa6f55be926a34d342f
mohaali250 <wsedrftgxcvb2@gmail.com>
2026-06-12 13:13:30 +0100

    Implement try-except in fetch_data
    Add error handling to fetch_data function

--- server/random_tips.py
+++ server/random_tips.py
@@ -16,6 +16,7 @@ from net.md_5.bungee.api.chat.hover.content import Text
 URL_DATA="https://raw.githubusercontent.com/mohaali250/purseWash-Mc/refs/heads/main/data/random_tips.json"
 
 def fetch_data():
+    try:
         return json.loads(response)
     except Exception as ex:
         Bukkit.getLogger().severe(
