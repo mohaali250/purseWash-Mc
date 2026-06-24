b34058f1888cf22f702e8983358a5033d90e84d8
mohaali250 <wsedrftgxcvb2@gmail.com>
2026-06-24 19:01:19 +0100

    Fix argument indexing in command handling

--- server/player_manager.py
+++ server/player_manager.py
@@ -1283,13 +1283,13 @@ def onTabComplete(sender,alias,args):
             return [" ".join(args[2:])] + reason_tab(args[2:])
     if len(args)>=3:
         if any([i==cmd for i in ["suspend","staffban","demote","warn"]]):
-            return [" ".join(args[3:])] + reason_tab(args[2:])
+            return [" ".join(args[3:])] + reason_tab(args[3:])
         if cmd=="punish":
             return typing_filter(args[-1],[gdata["punishments"][h]["meta"]["internal_name"] for h in gdata["punishments"].keys()])
         if cmd=="demote":
-            return [" ".join(args[3:])] + reason_tab(args[2:])
+            return [" ".join(args[3:])] + reason_tab(args[3:])
         if cmd=="warn":
-            return [" ".join(args[3:])] + reason_tab(args[2:])
+            return [" ".join(args[3:])] + reason_tab(args[3:])
         if cmd=="status" and args[0] == "set":
             # /status set <player>
             target = Bukkit.getOfflinePlayer(args[1])
